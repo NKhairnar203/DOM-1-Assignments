932214b1bc5be8caf99f50daf2f123763199a31e
Nilesh Khairnar <139966220+NKhairnar203@users.noreply.github.com>
2024-01-12 21:31:30 +0530

    Add files via upload

---
+++ Debauncing/script.js
@@ -0,0 +1,60 @@
+
+
+function movieRender(){   
+    let input_box = document.getElementById("input-box").value;
+    if (input_box == "") {
+        alert("please enter a valid Movie name!");
+    }
+    else {
+        let apiKey = "abd13736"
+        let apiurl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${input_box}`
+        
+        fetch(apiurl).then((res) =>
+            res.json()).then((final) =>
+                Display(final)
+            ).catch((error) =>
+                console.log(error))
+    }
+
+    Display()
+
+}
+
+
+
+function Display(movie){
+    let poster = document.querySelector("#poster");
+
+    let image = document.createElement('img');
+    image.src = movie.Poster
+    
+    let movieTitle = document.createElement('h2');
+    movieTitle.textContent = `Movie Title: ${movie.Title}`
+
+    let released = document.createElement('p');
+    released.textContent = `movie released data: ${movie.Released}`
+
+    let Director = document.createElement('p');
+    Director.textContent = `Movie Director : ${movie.Director}`
+
+    let Country = document.createElement('p');
+    Country.textContent = `Country : ${movie.Country}`
+
+    let Genre = document.createElement('p');
+    Genre.textContent = `Genre: ${movie.Genre}`
+
+    poster.append(image,movieTitle,released,Director,Country,Genre)
+
+   
+}
+
+let flag;
+function debauncing(call,delay){
+    if(flag){
+        clearTimeout(flag);
+
+    }
+    flag = setTimeout(() => {
+        call()
+    }, delay);
+}
